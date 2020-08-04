b05b50c413d08dae214e4cf555942fae78ed1030
Junaid <qjunaid623@gmail.com>
2020-08-04 15:23:50 +0500

    Added Animations

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -1,19 +1,58 @@
 import React from "react";
-import useWebAnimations, { bounce } from '@wellyshen/use-web-animations';
+import useWebAnimations from '@wellyshen/use-web-animations';
 import '../App.css';
+import dog from '../images/dog.gif';
+import cat from '../images/cat.gif';
+
 
 const Animations = () => {
 
-    const { ref, playState, getAnimation } = useWebAnimations({...bounce})
+    const { ref, playState, getAnimation } = useWebAnimations({
+        keyframes: {
+            transform: ['translate(0,0)'],
+            transform: ['translate(1000px,0)'],
+        },
+        timing: {
+            duration: 2000,
+            iterations: Infinity,
+            easing: 'ease-in-out',
+        },
+        autoPlay: false,
+        // playbackRate: 0.2,
+    })
+
+
+    const play = () => {
+        getAnimation().play();
+    };
+
+    const pause = () => {
+        getAnimation().pause();
+    };
+
+    const cancel = () => {
+        getAnimation().cancel();
+    };
+
+
 
     return (
-        <div>
-            <div className='target' ref={ref}>
-                Hello Junaid
+        <div className='main-container'>
+            <h1>Cat and Dog Animation</h1>
+            <button className='button' onClick={play}>Play</button>
+            <button className='button' onClick={pause}>Pause</button>
+            <button className='button' onClick={cancel}>Reset</button>
+
+            <div className='target' >
+                <div className='animation' >
+                    <div ref={ref} className='moving-container'>
+                        <img className='dogImage' src={dog} alt="" />
+                        <img className='catImage' src={cat} alt="" />
+                    </div>
+                </div>
             </div>
-            <h3>Current State: {playState}</h3>
-            <button className='button' onClick={() => getAnimation().play()}>Play</button>
-            <button className='button' onClick={() => getAnimation().pause()}>Pause</button>
+            <hr/>
+            <h1>Made by : <a href="https://github.com/developer-junaid" target='_blank'> Junaid Qureshi</a>  </h1><br/><br/>
         </div>
     )
 }
